ee2603c3b7fd831f2339ba8aa6578b2cf37bc446
Nicholas Carmen <NicholasJCarmen@gmail.com>
2016-12-07 23:59:20 -0500

    added documents

--- app/src/main/java/com/philips/lighting/quickstart/DataClass/Database/DBHelper.java
+++ app/src/main/java/com/philips/lighting/quickstart/DataClass/Database/DBHelper.java
@@ -47,34 +47,4 @@ public class DBHelper extends SQLiteOpenHelper {
         onCreate(db);
     }
 
-    /**
-     * Helper function that parses a given table into a string
-     * and returns it for easy printing. The string consists of
-     * the table name and then each row is iterated through with
-     * column_name: value pairs printed out.
-     *
-     * @param db the database to get the table from
-     * @param tableName the the name of the table to parse
-     * @return the table tableName as a string
-     */
-
-    //Error in this function. will only print if the table is made up of all strings
-    public String getTableAsString(SQLiteDatabase db, String tableName) {
-        Log.d(TAG, "getTableAsString called");
-        String tableString = String.format("Table %s:\n", tableName);
-        Cursor allRows  = db.rawQuery("SELECT * FROM " + tableName, null);
-        if (allRows.moveToFirst() ){
-            String[] columnNames = allRows.getColumnNames();
-            do {
-                for (String name: columnNames) {
-                    tableString += String.format("%s: %s\n", name,
-                            allRows.getString(allRows.getColumnIndex(name)));
-
-                }
-                tableString += "\n";
-            } while (allRows.moveToNext());
-        }
-        return tableString;
-    }
-
 }
